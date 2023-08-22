589a7485cda0948ab9d8847592febce82bd75b20
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-08-23 02:26:50 +0530

    connected backend user being added to database

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -10,7 +10,16 @@ const getPropertyDetails = async (req, res) => {
 };
 
 const createProperty = async (req, res) => {
+    try {
+        const {title, description, propertyType, price, location, photo, creator } = req.body;
+        const session = await mongoose.startSession();
+        session.startSession();
+        const user = await User.findOne({ email });
+        if (!user) throw new Error('User not found');
+        
+    } catch(error) {
 
+    }
 };
 
 const updateProperty = async (req, res) => {
