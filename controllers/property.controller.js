e78e0251cda4dc9c553ee63cd49d4a339853d515
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-08-22 23:14:24 +0530

    controllers and routes

---
+++ controllers/property.controller.js
@@ -0,0 +1,30 @@
+import Property from "../mongodb/models/property.js";
+import User from "../mongodb/models/user.js";
+
+const getAllProperty = async (req, res) => {
+
+};
+
+const getPropertyDetails = async (req, res) => {
+
+};
+
+const createProperty = async (req, res) => {
+
+};
+
+const updateProperty = async (req, res) => {
+
+};
+
+const deleteProperty = async (req, res) => {
+
+};
+
+export {
+    getAllProperty,
+    getPropertyDetails,
+    createProperty,
+    updateProperty,
+    deleteProperty,
+};
---
+++ controllers/user.controller.js
@@ -0,0 +1,24 @@
+import User from "../mongodb/models/user.js";
+
+const getAllUsers = async (req, res) => {
+
+};
+
+const createUser = async (req, res) => {
+
+};
+
+const getUserbyID = async (req, res) => {
+
+};
+
+const updateUserInfobyID = async (req, res) => {
+
+};
+
+export {
+    getAllUsers,
+    createUser,
+    getUserbyID,
+    updateUserInfobyID,
+}
