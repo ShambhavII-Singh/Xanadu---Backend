e9c75a3a578bb3bc4148fcb3f2ee24de19014308
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-08-27 19:26:52 +0530

    filteration, pagination, sorting

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -12,8 +12,15 @@ cloudinary.config({
 });
 
 const getAllProperties = async (req, res) => {
+    const { _start, _end, _order, _sort, title_like = "", propertyType = "" } = req.query;
+    const query = {};
+    if (propertyType !== "") {query.propertyType = propertyType};
+    if (title_like) {query.title = {$regex: title_like, $options: "i" }};
     try {
-        const properties = await Property.find({}).limit(req.query.end);
+        const count = await Property.countDocuments({ query });
+        const properties = await Property.find(query).limit(_end).skip(_start).sort({ [_sort]: _order });
+        res.header("x-total-count", count);
+        res.header("Access-Control-Expose-Headers", "x-total-count");
         res.status(200).json(properties);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -26,7 +33,7 @@ const getPropertyDetails = async (req, res) => {
 
 const createProperty = async (req, res) => {
     try {
-        const {title, description, propertyType, price, location, photo, email } = req.body;
+        const { title, description, propertyType, price, location, photo, email } = req.body;
         const user = await User.findOne({ email });
         if (!user) throw new Error('User not found');
         const photoURL = await cloudinary.uploader.upload(photo);
@@ -40,7 +47,7 @@ const createProperty = async (req, res) => {
             creator: user._id,
         });
         user.allProperties.push(newProperty._id);
-        res.status(304).json("Property creaed !!!");
+        res.status(304).json("Property created !!!");
     } catch(error) {
         res.status(500).json({ message: error.message });
     }
